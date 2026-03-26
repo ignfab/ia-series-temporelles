9ab9dbb4e7a432361a4b98c1c18d537f57e55c21
ElliotVincent <elt.vincent@gmail.com>
2026-03-26 18:15:15 +0100

    combine 2 modèles + optimisation des loops

--- unique_image.py
+++ unique_image.py
@@ -20,17 +20,26 @@ val_loader = DataLoader(
     val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS
 )
 
-model_name = "FLAIR-INC_rgb_15cl_resnet34-unet_weights.pth"
-model = smp.Unet(
+model_name_rgb = "FLAIR-INC_rgb_15cl_resnet34-unet_weights.pth"
+model_rgb = smp.Unet(
     encoder_name="resnet34", encoder_weights="imagenet", in_channels=3, classes=19
 )
-model.load_state_dict(
-    {key[16:]: val for key, val in torch.load(model_name).items() if len(key[16:]) > 0}
+model_rgb.load_state_dict(
+    {key[16:]: val for key, val in torch.load(model_name_rgb).items() if len(key[16:]) > 0}
+)
+
+model_name_rgbi = "FLAIR-INC_rgbi_15cl_resnet34-unet_weights.pth"
+model_rgbi = smp.Unet(
+    encoder_name="resnet34", encoder_weights="imagenet", in_channels=4, classes=19
+)
+model_rgbi.load_state_dict(
+    {key[16:]: val for key, val in torch.load(model_name_rgbi).items() if len(key[16:]) > 0}
 )
 
 
 # -- Phase évaluation --
-model.eval()
+model_rgb.eval()
+model_rgbi.eval()
 epoch_loss, epoch_acc, epoch_mae = 0.0, 0.0, 0.0
 
 couleurs_rvb_19_classes = torch.tensor(
@@ -62,33 +71,35 @@ with torch.no_grad():
         images = batch["images"]  # (B, T, 4, H, W)
         frame_id = batch["frame_id"]
         emprise = batch["emprise"]  # (B, H, W)
+        n_channels = batch["n_channels"]  # (B, T, 4)
         B, T, C, H, W = (
             images.shape
         )  # sauvegarder le batch size et la séquence temporelle
 
-        outputs = model(images.float().reshape(-1, C, H, W)[:, :3])  # (B*T, 19, H, W)
+        outputs_rgb = model_rgb(images.float().reshape(-1, C, H, W)[:, :3])  # (B*T, 19, H, W)
+        outputs_rgbi = model_rgbi(images.float().reshape(-1, C, H, W))  # (B*T, 19, H, W)
+        n_channels = n_channels.reshape(-1, 4)
+        outputs = torch.where((n_channels.sum(-1) == 4)[..., None, None, None], (outputs_rgbi + outputs_rgb)/2, outputs_rgb)
         preds = torch.argmax(outputs, dim=1)  # (B*T, H, W)
         preds = preds.reshape(B, T, H, W)  # (B, T, H, W)
         preds_emprise = (preds + 1) * emprise.unsqueeze(
             1
         )  # (B, T, H, W) — ne garder que les prédictions dans l'emprise
-        pred_frame_id = torch.zeros(B, dtype=torch.long)
-        for b in range(B):
-            for t in range(T):
-                indices_classes, hist = torch.unique(
-                    preds_emprise[b, t], return_counts=True
-                )
-                if 1 in indices_classes:
-                    proportion = (
-                        hist[indices_classes == 1].item()
-                        / emprise[b].sum().item()
-                        * 100
-                    )
-                    if (
-                        proportion >= SEUIL_BATIMENT
-                    ):  # seuil de 50% de pixels prédits comme bâtiment
-                        pred_frame_id[b] = t
-                        break
+
+        masque_classe1 = (preds_emprise == 1)  # B x T x H x W
+        nb_pixels_classe1 = masque_classe1.sum(dim=(-2, -1)).float()  # B x T
+        taille_emprise = emprise.sum(dim=(-2, -1)).float().unsqueeze(1)  # B x 1
+        proportions = nb_pixels_classe1 / taille_emprise * 100  # Proportion (%) par (b, t), shape : B x T
+        au_dessus_seuil = proportions >= SEUIL_BATIMENT
+        detection_trouvee = au_dessus_seuil.any(dim=1)  # B
+        premiere_detection = au_dessus_seuil.float().argmax(dim=1)  # B
+
+        pred_frame_id = torch.where(
+            detection_trouvee,
+            premiere_detection,
+            torch.zeros(B, dtype=torch.long)
+        )
+                
         # preds_rvb = couleurs_rvb_19_classes[preds]  # (B, T, H, W, 3)
         # print(f"preds_rvb.shape = {preds_rvb.shape}")
         # plt.imshow(preds_rvb[0, 0])
